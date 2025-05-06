2e0134cc38b0575f4f78d679628442aea83b8b85
hugues <36239975+huguesdevimeux@users.noreply.github.com>
2025-05-06 14:56:43 +0200

    Initial commit

---
+++ openapi/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi.api.admin_api import AdminApi
+from openapi.api.assignment_api import AssignmentApi
+from openapi.api.chat_api import ChatApi
+from openapi.api.feedback_api import FeedbackApi
+from openapi.api.form_api import FormApi
+from openapi.api.group_api import GroupApi
+from openapi.api.user_api import UserApi
+
